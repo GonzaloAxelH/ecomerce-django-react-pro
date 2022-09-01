c6fc5d35eb92d79f0f47912f1d42037830f07639
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-09-01 15:02:34 -0500

    asdasd

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,4 +1,4 @@
-from apps.product.views import ProductDetailView, ListProductView, ListBySearchView, ListRelatedView, ListSearchView, ArduinoSerialView, ResetSerial
+from apps.product.views import ProductDetailView, ListProductView, ListBySearchView, ListRelatedView, ListSearchView
 from django.urls import path
 app_name = "product"
 urlpatterns = [
@@ -7,6 +7,6 @@ urlpatterns = [
     path("search", ListSearchView.as_view()),
     path("related/<productId>", ListRelatedView.as_view()),
     path("by/search", ListBySearchView.as_view()),
-    path("arduino", ArduinoSerialView.as_view()),
-    path("reset-serial", ResetSerial.as_view())
+   # path("arduino", ArduinoSerialView.as_view()),
+  #  path("reset-serial", ResetSerial.as_view())
 ]
